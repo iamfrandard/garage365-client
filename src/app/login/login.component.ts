f2a060834f382df6d54211211560878c18f29a55
iamfrandard <danielchalasrd@gmail.com>
2023-11-22 00:40:18 -0400

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -53,7 +53,7 @@ export class LoginComponent implements OnInit {
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.storageService.getUser().roles;
-        this.router.navigate(['/reserva']);
+        this.router.navigate(['/reservas']);
       },
       error: (err) => {
         this.errorMessage = err.error.message;
