f1f63c39f64779db874d08ccb055f25c1f4b044f
luis cruz <luisangelc811@hotmail.com>
2023-11-24 02:26:17 -0400

    COMMIT

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -137,13 +137,14 @@ export class ChatBoxComponent implements OnInit, OnChanges {
       }
     );
   }
-  showNotification(message: ChatMessage) {
-    this.messageService.add({
-      severity: "info",
-      summary: `Nuevo mensaje de ${message.userName}`,
-      detail: message.content,
-    });
-  }
+
+  // showNotification(message: ChatMessage) {
+  //   this.messageService.add({
+  //     severity: "info",
+  //     summary: `Nuevo mensaje de ${message.userName}`,
+  //     detail: message.content,
+  //   });
+  // }
 
   // Envía un nuevo mensaje al servidor a través de Socket.io
   sendMessage() {
